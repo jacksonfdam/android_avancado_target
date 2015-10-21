ad89c611f049608278df3d8f5f27db13535dd698
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2015-10-21 06:02:08 -0200

    - New FAB - Custom

--- Exercicio05/app/src/main/java/br/com/targettrust/exercicio05/MainActivity.java
+++ Exercicio05/app/src/main/java/br/com/targettrust/exercicio05/MainActivity.java
@@ -1,10 +1,22 @@
-package br.com.targettrust.exercicio04;
+package br.com.targettrust.exercicio05;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.graphics.Outline;
+import android.graphics.drawable.RippleDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        ListView lv = (ListView) findViewById(R.id.lv1);
+        String[] data = new String[100];
+        for (int i=0; i < data.length; i++)
+            data[i] = "Data " + i;
+
+        ArrayAdapter<String> aAdpt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(aAdpt);
+/*
+        ImageButton ib = (ImageButton) findViewById(R.id.fab);
+        int diameter = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        System.out.println("Diameter ["+diameter+"]");
+        Outline outline = new Outline();
+        outline.setOval(0, 0, diameter, diameter);
+        ib.setOutline(outline);
+        ib.setClipToOutline(true);
+*/
+
+
+
+
     }
 
     @Override
