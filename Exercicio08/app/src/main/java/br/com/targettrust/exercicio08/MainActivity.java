59c73168ee9d55472fcbde68bd0863edaf311134
Jackson F. de A. Mafra <jackson.mafra@fullad.com.br>
2015-10-23 15:06:32 -0200

    - Memory Cache  - File Cache

--- Exercicio08/app/src/main/java/br/com/targettrust/exercicio08/MainActivity.java
+++ Exercicio08/app/src/main/java/br/com/targettrust/exercicio08/MainActivity.java
@@ -5,20 +5,31 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageView imgView;
+    private ImageLoader imgLoader;
+    private String strURL = "http://technotalkative.com/android.jpg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        imgView = (ImageView) findViewById(R.id.imageView1);
+        imgLoader = new ImageLoader(this);
+    }
+
+    public void btnLoadImageClick(View v){
+
+        imgLoader.DisplayImage(strURL, imgView);
     }
 
     @Override
